d6ec6ba8ff6db61f70eca4bd9dccb6a47196cab1
Elizabetht1 <eterveen@andrew.cmu.edu>
2025-11-08 14:14:54 -0500

    video specific

--- open-router.py
+++ open-router.py
@@ -5,16 +5,29 @@ if __name__ == "__main__":
     response = requests.post(
     url="https://openrouter.ai/api/v1/chat/completions",
     headers={
-        "Authorization": "Bearer <YOUR API KEY>",
+        "Authorization": "Bearer <TOKEN>",
         # "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
         # "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
     },
     data=json.dumps({
-        "model": "openai/gpt-4o", # Optional
+        "model": "google/gemini-2.5-flash-preview-09-2025", # Optional
         "messages": [
         {
             "role": "user",
-            "content": "What is the meaning of life?"
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Output the key frames"
+                },
+                # {
+                #     "type": "input_video",
+                #     "video_url": {
+                #       "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                #     }
+                # }
+                {"type": "input_video",
+                "video_url" : {"url": "https://www.youtube.com/watch?v=6SmLo1Td5zY"}}
+            ]
         }
         ]
     })
